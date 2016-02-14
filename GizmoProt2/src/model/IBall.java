013af256714d01c66a88beed7929c5cb21fdc49c
Bartosz Lewandowski <barteklewandowski94@gmail.com>
2016-02-14 16:05:54 +0000

    updated ball interface

--- GizmoProt2/src/model/IBall.java
+++ GizmoProt2/src/model/IBall.java
@@ -1,5 +1,25 @@
 package model;
 
-public interface IBall {
+import java.awt.Color;
 
+import physics.Circle;
+import physics.Vect;
+
+public interface IBall {
+	
+	public Vect getVelocity();
+	
+	public void setVelocity(Vect v);
+	
+	public double getRadius();
+	
+	public Circle getCircle();
+	
+	public boolean isMoving();
+	
+	public void setMoving(boolean b);
+	
+	public Color getColor();
+	
+	public void setColor(Color c);
 }
