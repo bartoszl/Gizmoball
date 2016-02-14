2c359c88d6ba093fe963611057d98e2dd844f9be
Bartosz Lewandowski <barteklewandowski94@gmail.com>
2016-02-14 16:02:24 +0000

    created a constructor, getters and setters for ball

--- GizmoProt2/src/model/Ball.java
+++ GizmoProt2/src/model/Ball.java
@@ -1,4 +1,57 @@
 package model;
+
+import java.awt.Color;
+
+import physics.Circle;
+import physics.Vect;
+
 public class Ball implements IBall {
-	private Vect
+	private Vect velocity;
+	private double radius;
+	private double xpos;
+	private double ypos;
+	private Color color;
+	
+	private boolean moving;
+	
+	public Ball(double xpos, double ypos, double xv, double yv){
+		this.xpos =xpos;
+		this.ypos =ypos;
+		velocity = new Vect(xv, yv);
+		color = Color.YELLOW;
+		radius = 10;
+		moving = true;
+	}
+	
+	public Vect getVelocity(){
+		return velocity;
+	}
+	
+	public void setVelocity(Vect v){
+		velocity = v;
+	}
+	
+	public double getRadius(){
+		return radius;
+	}
+	
+	public Circle getCircle(){
+		return new Circle(xpos, ypos, radius);
+	}
+	
+	public boolean isMoving(){
+		return moving;
+	}
+	
+	public void setMoving(boolean b){
+		moving = b;
+	}
+	
+	public Color getColor(){
+		return color;
+	}
+	
+	public void setColor(Color c){
+		color = c;
+	}
 }
