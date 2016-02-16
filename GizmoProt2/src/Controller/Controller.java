030d8dc49925bcc52509096bcf8b163ffbf5cfdc
Martin Peev <qmmmmmm3@gmail.com>
2016-02-16 21:36:27 +0000

    added addBall and addAbsorber as well as started the controller. Added some minor changes to absorber and ball (getters)

---
+++ GizmoProt2/src/Controller/Controller.java
@@ -0,0 +1,32 @@
+package Controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import View.RunGUI;
+import model.Ball;
+
+public class Controller implements IController{
+	private RunGUI gui;
+	
+	public Controller(RunGUI gui){
+		this.gui=gui;
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		String command = e.getActionCommand();
+		// handle close
+
+		switch (command) {
+		case "add ball":
+			addBall();
+			break;
+		case "exit":
+			System.exit(0);
+		}
+	}
+
+	private void addBall() {
+		gui.addBallToGrid(new Ball(200,200,100,0));
+	}
+}
