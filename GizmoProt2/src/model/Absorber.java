0e04c219b88918fa8b4ec545ebb3255e2f82e89a
stevycd <stephen.dundas@live.co.uk>
2016-02-18 17:49:21 +0000

    KeyListener now working, Default fire key set to 'F'.

--- GizmoProt2/src/model/Absorber.java
+++ GizmoProt2/src/model/Absorber.java
@@ -65,10 +65,10 @@ public class Absorber implements IAbsorber {
 	public void absorb(Ball ball){
 		//ball.setMoving(false);
 		ball.setXY(xBottomRight-20, yBottomRight-20);
-		Vect v = new Vect(0,-1000);
+		Vect v = new Vect(0,-950);
 		ball.setVelocity(v);
 		ball.setAbsorbed(true);
-		System.out.println("ABSORBED");
+		//System.out.println("ABSORBED");
 	}
 	
 	public Color getColor(){
