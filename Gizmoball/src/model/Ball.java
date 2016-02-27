191f5ad3cda2daef994cf3795fa6eb7881da2168
Bartosz Lewandowski <barteklewandowski94@gmail.com>
2016-02-27 19:03:35 +0000

    added IBall interface and Ball class to the model, along with javadocs.

--- Gizmoball/src/model/Ball.java
+++ Gizmoball/src/model/Ball.java
@@ -1,5 +1,118 @@
 package model;
 
-public class Ball {
+import physics.Circle;
+import physics.Vect;
+import java.awt.Color;
+
+/**
+ * 
+ * @author Bartosz Lewandowski
+ *
+ * Part of the Gizmoball model representing a Ball. 
+ * Implements IBall interface. 
+ */
+
+public class Ball implements IBall{
+	private Vect velocity;
+	private double x, y, r;
+	private Color color;
+	private boolean moving, absorbed;
 	
+	/**
+	 * Constructor for Ball object. Initially sets color to Yellow,
+	 * Radius to 5px, marks ball as moving and sets the ball as 
+	 * not absorbed.
+	 * 
+	 * @param x -> double, representing Ball's position along x axis
+	 * @param y -> double, representing Ball's position along y axis
+	 * @param xv -> double, representing velocity along x axis
+	 * @param yv -> double, representing velocity along y axis
+	 */
+	public Ball(double x, double y, double xv, double yv){
+		this.x = x;
+		this.y = y;
+		this.velocity = new Vect(xv, yv);
+		this.color = Color.YELLOW;
+		this.r = 5;
+		moving = true;
+		absorbed = false;
+	}
+	
+	@Override
+	public Vect getVelocity() {
+		return velocity;
+	}
+	
+	@Override
+	public void setVelocity(Vect v) {
+		velocity = v;
+	}
+	
+	@Override
+	public void setVelocity(double vx, double vy) {
+		velocity = new Vect(vx, vy);
+	}
+	
+	@Override
+	public double getX() {
+		return x;
+	}
+	
+	@Override
+	public double getY() {
+		return y;
+	}
+	
+	@Override
+	public void setXY(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	@Override
+	public Color getColor() {
+		return color;
+	}
+	
+	@Override
+	public void setColor(Color color) {
+		this.color = color;
+		
+	}
+	
+	@Override
+	public boolean isMoving() {
+		return moving;
+	}
+	
+	@Override
+	public void setMoving(boolean b) {
+		moving = b;
+	}
+	
+	@Override
+	public boolean isAbsorbed() {
+		return absorbed;
+	}
+	
+	@Override
+	public void setAbsorbed(boolean b) {
+		absorbed = b;
+	}
+
+	@Override
+	public double getRadius() {
+		return r;
+	}
+
+	@Override
+	public void setRadius(double r) {
+		this.r = r;
+	}
+	
+	@Override
+	public Circle getCircle() {
+		return new Circle(x,y,r);
+	}
+
 }
--- Gizmoball/src/model/IBall.java
+++ Gizmoball/src/model/IBall.java
@@ -1,5 +1,121 @@
 package model;
 
-public interface IBall {
+import physics.Circle;
+import physics.Vect;
+import java.awt.Color;
 
+/**
+ * 
+ * @author Bartosz Lewandowski
+ * 
+ * Interface representing Ball in the Gizmoball model.
+ */
+public interface IBall {
+	
+	/**
+	 * Getter method for velocity.
+	 * 
+	 * @return Vect, representing velocity of the ball.
+	 */
+	public Vect getVelocity();
+	
+	/**
+	 * Setter method for velocity.
+	 * 
+	 * @param v -> Vect, representing new velocity of the Ball.
+	 */
+	public void setVelocity(Vect v);
+	
+	/**
+	 * Setter method for velocity.
+	 * 
+	 * @param vx -> double, representing new velocity along X axis.
+	 * @param vy -> double, representing new velocity along Y axis.
+	 */
+	public void setVelocity(double vx, double vy);
+	
+	/**
+	 * Getter method for x coordinate.
+	 * 
+	 * @return double, X coordinate of the Ball.
+	 */
+	public double getX();
+	
+	/**
+	 * Getter method for y coordinate.
+	 * 
+	 * @return double, Y coordinate of the Ball
+	 */
+	public double getY();
+	
+	/**
+	 * Setter method for X and Y coordinates.
+	 * 
+	 * @param x -> double, new X coordinate of the ball.
+	 * @param y -> double, new Y coordinate of the ball.
+	 */
+	public void setXY(double x, double y);
+	
+	/**
+	 * Getter method for Color.
+	 * 
+	 * @return -> Color, representing Color of the ball.
+	 */
+	public Color getColor();
+	
+	/**
+	 * Setter method for Color
+	 * 
+	 * @param color -> Color, new color of the Ball.
+	 */
+	public void setColor(Color color);
+	
+	/**
+	 * Getter method for radius.
+	 * 
+	 * @return double, the radius of the Ball.
+	 */
+	public double getRadius();
+	
+	/**
+	 * Setter method for radius
+	 * 
+	 * @param r -> double, new radius of the ball.
+	 */
+	public void setRadius(double r);
+	
+	/**
+	 * Getter method for moving.
+	 * 
+	 * @return boolean, showing whether the ball is moving or not.
+	 */
+	public boolean isMoving();
+	
+	/**
+	 * Setter method for moving.
+	 * 
+	 * @param b -> boolean, new value for moving.
+	 */
+	public void setMoving(boolean b);
+	
+	/**
+	 * Getter method for absorbed.
+	 * 
+	 * @return boolean, showing whether the ball is absorbed or not.
+	 */
+	public boolean isAbsorbed();
+	
+	/**
+	 * Setter method for absorber.
+	 * 
+	 * @param b -> boolean, new value for absorbed.
+	 */
+	public void setAbsorbed(boolean b);
+	
+	/**
+	 * Helper method, that returns a circle representing the Ball.
+	 * 
+	 * @return Circle, representing the ball.
+	 */
+	public Circle getCircle();
 }
