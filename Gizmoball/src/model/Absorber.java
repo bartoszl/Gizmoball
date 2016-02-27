e2ea0366f5948bcde4aea62476747aef91957b19
Bartosz Lewandowski <barteklewandowski94@gmail.com>
2016-02-27 21:42:47 +0000

    added some methods to iabsorber and absorber, mostly getters

--- Gizmoball/src/model/Absorber.java
+++ Gizmoball/src/model/Absorber.java
@@ -1,5 +1,74 @@
 package model;
 
-public class Absorber implements IAbsorber {
+import java.awt.Color;
+import java.util.List;
 
+import physics.Circle;
+import physics.LineSegment;
+
+public class Absorber implements IAbsorber {
+	private double xTopLeft, yTopLeft, xBottomRight, yBottomRight;
+	private Color color;
+	
+	public Absorber(double x, double y, double x1, double y1){
+		xTopLeft=x;
+		yTopLeft=y;
+		xBottomRight=x1;
+		yBottomRight=y1;
+		color=Color.PINK;
+	}
+
+	@Override
+	public double getXTopLeft() {
+		return xTopLeft;
+	}
+
+	@Override
+	public double getYTopLeft() {
+		return yTopLeft;
+	}
+
+	@Override
+	public double getXBottomRight() {
+		return xBottomRight;
+	}
+
+	@Override
+	public double getYBottomRight() {
+		return yBottomRight;
+	}
+
+	@Override
+	public double getWidth() {
+		return xBottomRight-xTopLeft;
+	}
+
+	@Override
+	public double getHeight() {
+		return yBottomRight-yTopLeft;
+	}
+
+	@Override
+	public List<LineSegment> getLines() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Circle> getCircles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Color getColor() {
+		return color;
+	}
+
+	@Override
+	public void absorb(Ball ball) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
--- Gizmoball/src/model/IAbsorber.java
+++ Gizmoball/src/model/IAbsorber.java
@@ -1,5 +1,30 @@
 package model;
 
-public interface IAbsorber {
+import physics.Circle;
+import physics.LineSegment;
+import java.util.*;
+import java.awt.Color;
 
+public interface IAbsorber {
+	
+	public double getXTopLeft();
+	
+	public double getYTopLeft();
+	
+	public double getXBottomRight();
+	
+	public double getYBottomRight();
+	
+	public double getWidth();
+	
+	public double getHeight();
+	
+	public List<LineSegment> getLines();
+	
+	public List<Circle> getCircles();
+	
+	public Color getColor();
+	
+	public void absorb(Ball ball);
+	
 }
