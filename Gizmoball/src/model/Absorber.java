2f83533259e585c6b7215a1d6354f70578f57ef6
Bartosz Lewandowski <barteklewandowski94@gmail.com>
2016-02-28 17:32:36 +0000

    Finished all the metods in absorber, and added javadocs

--- Gizmoball/src/model/Absorber.java
+++ Gizmoball/src/model/Absorber.java
@@ -1,15 +1,31 @@
 package model;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.List;
 
 import physics.Circle;
 import physics.LineSegment;
 
+/**
+ * 
+ * @author Bartosz Lewandowski
+ * 
+ * Part of the Gimozball model, representing the Absorber.
+ * Implements IAbsorber interface.
+ *
+ */
 public class Absorber implements IAbsorber {
 	private double xTopLeft, yTopLeft, xBottomRight, yBottomRight;
 	private Color color;
-	
+	/**
+	 * Constructor for Absorber object. Initially sets color to Pink
+	 * 
+	 * @param x -> double, representing x coordinate of top left corner of the absorber.
+	 * @param y -> double, representing y coordinate of top left corner of the absorber.
+	 * @param x1 -> double, representing x coordinate of bottom right corner of the absorber.
+	 * @param y1 -> double, representing y coordinate of the bottom right corner of the absorber.
+	 */
 	public Absorber(double x, double y, double x1, double y1){
 		xTopLeft=x;
 		yTopLeft=y;
@@ -50,14 +66,30 @@ public class Absorber implements IAbsorber {
 
 	@Override
 	public List<LineSegment> getLines() {
-		// TODO Auto-generated method stub
-		return null;
+		List<LineSegment> l = new ArrayList<LineSegment>();
+		LineSegment l1 = new LineSegment(xTopLeft, yTopLeft, xBottomRight, yTopLeft);
+		LineSegment l2 = new LineSegment(xBottomRight, yTopLeft, xBottomRight, yBottomRight);
+		LineSegment l3 = new LineSegment(xTopLeft, yBottomRight, xBottomRight, yBottomRight);
+		LineSegment l4 = new LineSegment(xTopLeft, yTopLeft, xTopLeft, yBottomRight);
+		l.add(l1);
+		l.add(l2);
+		l.add(l3);
+		l.add(l4);
+		return l;
 	}
 
 	@Override
 	public List<Circle> getCircles() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Circle> l = new ArrayList<Circle>();
+		Circle c1 = new Circle(xTopLeft, yTopLeft, 0);
+		Circle c2 = new Circle(xBottomRight, yTopLeft, 0);
+		Circle c3 = new Circle(xBottomRight, yBottomRight, 0);
+		Circle c4 = new Circle(xTopLeft, yBottomRight, 0);
+		l.add(c1);
+		l.add(c2);
+		l.add(c3);
+		l.add(c4);
+		return l;
 	}
 
 	@Override
@@ -66,9 +98,15 @@ public class Absorber implements IAbsorber {
 	}
 
 	@Override
+	public void setColor(Color color) {
+		this.color=color;
+	}
+	
+	@Override
 	public void absorb(Ball ball) {
-		// TODO Auto-generated method stub
-		
+		ball.setAbsorbed(true);
+		ball.setVelocity(0, -1000);
+		ball.setXY(xBottomRight-15, yBottomRight-15);
 	}
 	
 }
--- Gizmoball/src/model/IAbsorber.java
+++ Gizmoball/src/model/IAbsorber.java
@@ -5,26 +5,104 @@ import physics.LineSegment;
 import java.util.*;
 import java.awt.Color;
 
+/**
+ * 
+ * @author Bartosz Lewandowski
+ * 
+ * Interface for Absorber class.
+ */
 public interface IAbsorber {
 	
+	/**
+	 * Getter method for x coordinate of the top left corner of the absorber.
+	 * 
+	 * @return double, representing x coordinate of the top left corner of the absorber.
+	 */
 	public double getXTopLeft();
 	
+	/**
+	 * Getter method for y coordinate of the top left corner of the absorber.
+	 * 
+	 * @return double, representing y coordinate of the top left corner of the absorber.
+	 */
 	public double getYTopLeft();
 	
+	/**
+	 * Getter method for x coordinate of the bottom right corner of the absorber.
+	 * 
+	 * @return double, representing x coordinate of the bottom right corner of the absorber.
+	 */
 	public double getXBottomRight();
 	
+	/**
+	 * Getter method for y coordinate of the bottom right corner of the absorber.
+	 * 
+	 * @return double, representing y coordinate of the bottom right corner of the absorber.
+	 */
 	public double getYBottomRight();
 	
+	/**
+	 * Method calculating the width for the absorber. This is a helper method
+	 * to simplify drawing in the GUI. The width is calculated by subtracting
+	 * top left x coordinate from bottom right x coordinate. 
+	 * 
+	 * @return double, representing the width of the absorber
+	 */
 	public double getWidth();
 	
+	/**
+	 * Method calculating the height for the absorber. This is a helper method
+	 * to simplify drawing in the GUI. The height is calculated by subtracting
+	 * top left y coordinate from bottom right y coordinate. 
+	 * 
+	 * @return double, representing the height of the absorber
+	 */
 	public double getHeight();
 	
+	/**
+	 * Method returning 4 line segments, representing the boundaries of the 
+	 * absorber. This method is useful for calculating collisions.
+	 * 
+	 * @return List<LineSegments> containing 4 LineSegments, representing
+	 * 		   top, right, bottom and left edge of the absorber.
+	 */
 	public List<LineSegment> getLines();
 	
+	/**
+	 * Method returning 4 Circles representing 4 corners of the absorber.
+	 * Radius of these Circles is set to 0. This method is useful for calculating 
+	 * collisions.
+	 * 
+	 * @return List<Circle> containing 4 Circles, representing top left,
+	 * 		   top right, bottom right and bottom left corner of the absorber.
+	 */
 	public List<Circle> getCircles();
 	
+	/**
+	 * Getter method for color of the absorber.
+	 * 
+	 * @return Color, representing the color of the absorber.
+	 */
 	public Color getColor();
 	
+	/**
+	 * Setter method for color.
+	 * 
+	 * @param color -> Instance of the Color class, which represents the new color
+	 * 				   of the absorber.
+	 */
+	public void setColor(Color color);
+	
+	/**
+	 * Method that should be called once the ball collides with the absorber.
+	 * Sets the ball speed to -25*L. Sets the position of the ball to the
+	 * Bottom left corner of the absorber and sets the absorbed field of the 
+	 * ball to true. This field should be set to false, once tha ball escapes the 
+	 * absorber.
+	 * 
+	 * @param ball -> Ball object representing a ball that just collided with
+	 * 				  the absorber.
+	 */
 	public void absorb(Ball ball);
 	
 }
