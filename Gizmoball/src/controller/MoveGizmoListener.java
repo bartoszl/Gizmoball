35b1a76f2ca690bbf850ac06ad35b7aad13db376
stevycd <stephen.dundas@live.co.uk>
2016-03-05 00:09:48 +0000

    Added the basis for some of the missing controllers.

---
+++ Gizmoball/src/controller/MoveGizmoListener.java
@@ -0,0 +1,63 @@
+package controller;
+
+import model.IGBallModel;
+import view.Board;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+
+public class MoveGizmoListener implements MouseListener {
+    private Board board;
+    private IGBallModel model;
+    private int[] clicks;
+    private boolean first;
+
+    public MoveGizmoListener(Board board, IGBallModel model){
+        this.board = board;
+        this.model = model;
+        first = true;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        Point mouseP = MouseInfo.getPointerInfo().getLocation();
+        Point gridP = board.getLocationOnScreen();
+        int x = mouseP.x - gridP.x;
+        int y = mouseP.y - gridP.y;
+        if(board.getMoving()) {
+            if(first){
+                clicks[0] = x;
+                clicks[1] = y;
+                first = false;
+            }
+            else {
+                clicks[2] = x;
+                clicks[3] = y;
+                model.moveElement(clicks[0],clicks[1],clicks[2],clicks[3]);
+                first = true;
+            }
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
