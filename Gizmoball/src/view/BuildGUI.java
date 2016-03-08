f83865e799315edef896cf9280a38051c4afd3f4
Martin Peev <qmmmmmm3@gmail.com>
2016-03-08 20:27:34 +0000

    GUI code improvements

--- Gizmoball/src/view/BuildGUI.java
+++ Gizmoball/src/view/BuildGUI.java
@@ -33,14 +33,7 @@ public class BuildGUI implements IGUI{
 	 * Create the application.
 	 */
 	public BuildGUI(Main main, IGBallModel model) {
-        this.addGizmo = new AddGizmoListener(this, model);
-        this.addBall = new AddBallListener(this, model);
-        this.addFlipper = new AddFlipperListener(this, model);
-        this.rotateComponent = new RotateComponentListener(this, model);
-        this.deleteComponent = new DeleteComponentListener(this, model);
-        this.main = main;
-        this.model = model;
-        this.board = new BuildBoard(model);
+        constructor(main, model);
         createFrame();
 		initialize();
 	}
@@ -49,18 +42,21 @@ public class BuildGUI implements IGUI{
 	 * Alternate constructor that takes in a JFrame object
 	 */
 	public BuildGUI(Main main, IGBallModel model, JFrame frame) {
-        this.addGizmo = new AddGizmoListener(this, model);
+		constructor(main, model);
+        this.frame = frame;
+		initialize();
+		frame.repaint();
+	}
+	
+	private void constructor(Main main, IGBallModel model){
+		this.addGizmo = new AddGizmoListener(this, model);
         this.addBall = new AddBallListener(this, model);
-        this.rotateComponent = new RotateComponentListener(this, model);
         this.addFlipper = new AddFlipperListener(this, model);
+        this.rotateComponent = new RotateComponentListener(this, model);
         this.deleteComponent = new DeleteComponentListener(this, model);
         this.main = main;
         this.model = model;
-        this.frame = frame;
-
         this.board = new BuildBoard(model);
-		initialize();
-		frame.repaint();
 	}
 
     public Board getGridView() {
@@ -177,6 +173,11 @@ public class BuildGUI implements IGUI{
                 board.removeMouseListener(rotateComponent);
                 board.removeMouseListener(deleteComponent);
             	board = null;
+                addGizmo = null;
+                addBall = null;
+                addFlipper = null;
+                rotateComponent = null;
+                deleteComponent = null;
             	frame.remove(frame.getContentPane());
             	frame.remove(frame.getJMenuBar());
             	frame.remove(panel);
