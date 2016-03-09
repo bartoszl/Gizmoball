fffc4d7b7d009db2d12bd4e65ccea458f872f301
John Watt <john.m.watt@hotmail.co.uk>
2016-03-09 23:18:41 +0000

    Added un-committed changes before getting the most recent changes from master.

--- Gizmoball/src/model/Writer.java
+++ Gizmoball/src/model/Writer.java
@@ -61,8 +61,6 @@ public class Writer {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 
     public List<String> convertAbsorberToFileSyntax(IAbsorber absorber) {
