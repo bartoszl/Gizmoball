f426b52c9a0f4cf6980e11a1c501403f6336a6c3
John Watt <john.m.watt@hotmail.co.uk>
2016-03-10 07:30:47 +0000

    Set main method back to the normal to launch the GUI.

--- Gizmoball/src/view/Main.java
+++ Gizmoball/src/view/Main.java
@@ -33,7 +33,7 @@ public class Main {
 
     public static void main(String[] args) {
     	
-        /*EventQueue.invokeLater(new Runnable() {
+        EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
                     Main m = new Main();
@@ -41,17 +41,17 @@ public class Main {
                     e.printStackTrace();
                 }
             }
-        });*/
-        GBallModel m = new GBallModel();
-        Writer writer = new Writer();
+        });
+        /*GBallModel m = new GBallModel();
+        Writer writer = new Writer();*/
         //m.addCircularBumper(40, 40, 0, "Circle1");
         /*m.addSquareBumper(80, 80, 0, "Square1");
         m.addAbsorber("Absorber1", 100, 100, 200, 200);
         m.addBall("Ball1", 60, 60, 50, 50);
         m.rotateElement(40, 40);*/
-        CircularBumper cBumper = new CircularBumper(100, 200, 0, "C1");
+        /*CircularBumper cBumper = new CircularBumper(100, 200, 0, "C1");
         Flipper flipper = new Flipper(9, 2, true, Color.RED, "LF92");
         m.addConnection(new Connection(cBumper, flipper));
-        writer.writeModelToFile(m, "Save1");
+        writer.writeModelToFile(m, "Save1");*/
     }
 }
