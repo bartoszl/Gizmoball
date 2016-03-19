d271d4d9ed55237193d277adaf84fad34ce34c89
Bartosz Lewandowski <barteklewandowski94@gmail.com>
2016-03-19 17:34:36 +0000

    cleaned up connections and added javadocs

--- Gizmoball/src/model/Connection.java
+++ Gizmoball/src/model/Connection.java
@@ -1,22 +1,50 @@
 package model;
 
+/**
+ * 
+ * @author Kiril Rupasov
+ *
+ */
 public class Connection {
 
     private CircularBumper trigger;
     private Flipper flipper;
-
+    
+    /**
+     * Constructor for connection.
+     * 
+     * @param trigger -> CircularBumper which will trigger an action in the connected flipper.
+     * @param flipper -> Flipper, flipper that will be triggered after the CircularBumper gets hit.
+     */
     public Connection(CircularBumper trigger, Flipper flipper) {
         this.trigger = trigger;
         this.flipper = flipper;
     }
-
+    
+    /**
+     * Getter method for trigger.
+     * 
+     * @return Circular Bumper, representing the trigger.
+     */
     public CircularBumper getTrigger() {
         return trigger;
     }
-
+    
+    /**
+     * Getter method for flipper.
+     * 
+     * @return Flipper, representing flipper which action will be triggered.
+     */
     public Flipper getFlipper() {
         return flipper;
     }
-
-    public void setFlipper(Flipper flipper) { this.flipper = flipper; }
+    
+    /**
+     * Setter method for flipper.
+     * 
+     * @param flipper -> Flipper, representing a flipper which action will be triggered.
+     */
+    public void setFlipper(Flipper flipper) {
+    	this.flipper = flipper; 
+    }
 }
